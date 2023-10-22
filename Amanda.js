e6de70308ef1be00208df32948bc501fdb88fde7
The last guest <148704333+resettingdorenato@users.noreply.github.com>
2023-10-22 16:36:36 -0300

    I'm dumb :p

---
+++ Amanda.js
@@ -0,0 +1,43 @@
+import redditor
+  from redditorr
+
+createvariable{amandacakecounter}
+
+
+
+end
+
+import reddits
+  from teletron
+
+createvariable{amandacookiecounter}
+
+if import reddits
+
+do discordo
+
+do again
+
+
+
+i drink a coffee like a adult
+
+
+
+
+
+import blacks
+
+
+  from whites
+
+
+Brazilian GUY!!!
+
+
+import color
+  from scratch
+
+imported from scratch
+
+
